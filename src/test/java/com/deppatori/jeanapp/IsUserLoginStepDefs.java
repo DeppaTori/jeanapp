7bba2e78fa89744e97fbb0e796ea377ca6f1f5bd
Tommy Toban <tommy.deppatori@gmail.com>
2019-06-24 16:45:39 +0700

    modi userlogin step def

--- src/test/java/com/deppatori/jeanapp/IsUserLoginStepDefs.java
+++ src/test/java/com/deppatori/jeanapp/IsUserLoginStepDefs.java
@@ -5,24 +5,32 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+import static org.junit.Assert.assertEquals;
+
+class IsUserLogin {
+    static String check(String status) {
+        return "logged".equals(status)?"USER LOGGED":"false";
+    }
+}
+
 public class IsUserLoginStepDefs{
 
+    private String loginStatus;
+    private String actualCheck;
+
     @Given("^Session is \"([^\"]*)\"$")
-    public void session_is(String arg1) throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void session_is(String sessionStatus) throws Exception {
+        loginStatus = sessionStatus;
     }
 
     @When("^System check whether user login yet$")
     public void system_check_whether_user_login_yet() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        actualCheck = IsUserLogin.check(loginStatus);
     }
 
     @Then("^Application should print \"([^\"]*)\"$")
-    public void application_should_print(String arg1) throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void application_should_print(String expectedAnswer) throws Exception {
+        assertEquals(expectedAnswer,actualCheck);
     }
 
 
